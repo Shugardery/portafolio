f1563ae2c88fdf02e6f263fe2d236452caa50492
shugardery <senkucotiart@gmail.com>
2022-03-27 14:49:05 -0500

    arreglos a formulario

--- script.js
+++ script.js
@@ -19,35 +19,37 @@
 })(document);
 
 /* *** ContactForm *** */
-((d)=> {
+((d) => {
   const $form = d.querySelector(".contact-form"),
     $loader = d.querySelector(".contact-form-loader"),
     $response = d.querySelector(".contact-form-response");
 
-
-  $form.addEventListener("submit" , (e) => {
-    e.PreventDefault();
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
     $loader.classList.remove("none");
     fetch("https://formsubmit.co/ajax/senkucotiart@gmail.com", {
-    method: "POST",
-    body: new FormData(e.target),
-  })
-    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-    .then((json) => {
-      console.log(json);
-      location.hash = "#gracias";
-      $form.reset();
+      method: "POST",
+      body: new FormData(e.target),
     })
-    .catch((err) => {
-      console.log(err);
-      let message = 
-        err.statusText || "Ocurrió un error al enviar. Intenta nuevamente";
-      $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
-    }).finally(()=> {
-      $loader.classList.add("none");
-      setTimeout(() => {
-        location.hash = "#close";
-      }, 3000);
-    });
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
   });
 })(document);
